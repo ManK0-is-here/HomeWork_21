a977ca13818c82c7fc5b6d41a7747c2740473238
ManK0-is-here <zarennyjracok@gmail.com>
2025-06-19 19:41:12 +0300

    HW_21: Неполноценный финал

--- barbershop/settings.py
+++ barbershop/settings.py
@@ -12,8 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
-from dotenv import load_dotenv
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+from dotenv import load_dotenv 
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "core"
 ]
 
 MIDDLEWARE = [
